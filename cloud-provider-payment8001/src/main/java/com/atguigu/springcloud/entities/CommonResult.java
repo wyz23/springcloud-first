6a6d40802cae627c7fd72096cb84b748b2d25ede
wyz23 <1156489892@qq.com>
2020-10-06 12:02:38 +0800

    init

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/entities/CommonResult.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor//全参构造
+@NoArgsConstructor//空参构造
+public class CommonResult<T> {
+    private Integer code;
+    private String  message;
+    private T       data;
+
+    public CommonResult(Integer code, String message) {
+//        this.code = code;
+//        this.message = message;
+        this(code, message, null);
+    }
+//    public CommonResult(Integer code, String message, T data) {
+//        this.code = code;
+//        this.message = message;
+//        this.data = data;
+//    }
+}
